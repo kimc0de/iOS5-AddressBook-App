43f3b91cc6b5c23c81e5a597356cc06d24b530e1
Kim Ngan <kimngan.ledang@gmail.com>
2020-12-15 23:39:01 +0100

    Create main.storyboard, connect with  Table View Controller classes, Table View Cell classes

---
+++ iOS5-LeDang-Pham/Controller/ContactListVC.swift
@@ -0,0 +1,29 @@
+//
+//  ContactListVC.swift
+//  iOS5-LeDang-Pham
+//
+//  Created by Kim on 15.12.20.
+//
+
+import UIKit
+
+class ContactListVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
